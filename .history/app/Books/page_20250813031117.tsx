7b421f028705c6db4043ed83de164c608337def2
EmmyDunny1 <emmanueloluwadunsin@gmail.com>
2025-08-13 04:50:33 +0100

    created and completed Books page, BookLaunch component, PreOrderPayment page

---
+++ .history/app/Books/page_20250813031117.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BooksCarousel from '@/components/BooksCarousel'
+
+export default function BooksPage() {
+  return (
+    <div>
+      <BooksCarousel />
+      
+    </div>
+  )
+}
---
+++ .history/app/Books/page_20250813033104.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BooksCarousel from '@/components/BooksCarousel'
+import PreOrderPayment from '../PreOrderPayment/page'
+
+export default function BooksPage() {
+  return (
+    <div>
+      <BooksCarousel />
+     
+    </div>
+  )
+}
